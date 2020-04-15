78db2b7e81662cc750cc60213479aca8e3fe19d9
mithicher <mithnehu@gmail.com>
2020-04-15 13:28:48 +0530

    Modal bug fixed again

--- sw.js
+++ sw.js
@@ -2,12 +2,28 @@ self.addEventListener('install', (event) => {
 	event.waitUntil(
 	  caches.open('v1').then((cache) => {
 		return cache.addAll([
+		 'index.html',
+		  './',
 		  './tailwind.css',
 		  './alpine.js'
 		]);
 	  })
 	);
 });
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+	const currentCaches = ['v1', 'runtime'];
+	event.waitUntil(
+	  caches.keys().then(cacheNames => {
+		return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+	  }).then(cachesToDelete => {
+		return Promise.all(cachesToDelete.map(cacheToDelete => {
+		  return caches.delete(cacheToDelete);
+		}));
+	  }).then(() => self.clients.claim())
+	);
+  });
 
 self.addEventListener('fetch', function(event) {
 	event.respondWith(caches.match(event.request).then(function(response) {
